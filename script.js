a99e69099ce0c9caba3bb4bb2ec2af3da9221286
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-01-10 17:03:59 -0300

    Add hamburger mobile menu with js

---
+++ script.js
@@ -0,0 +1,41 @@
+const menuButton = document.querySelector(".hamburger-btn");
+const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".close");
+const suscribeButton = document.querySelector(".suscribe-btn");
+const navBar = document.querySelector(".nav-bar");
+const navList = document.querySelector(".nav-ul");
+
+menuButton.addEventListener("click", showMenu);
+
+function changeNavBarStyles() {
+  navBar.style.position = "fixed";
+  navBar.style.flexDirection = "column";
+  navBar.style.alignItems = "center";
+  navBar.style.justifyContent = "center";
+  navBar.style.backgroundColor = "rgb(29, 29, 29)";
+  navBar.style.width = "93%";
+  navBar.style.height = "260px";
+  navBar.style.textAlign = "center";
+  navBar.style.transform = "translateY(12px)";
+  navList.style.flexDirection = "column";
+  suscribeButton.style.padding = "1.5rem";
+  suscribeButton.style.paddingTop = "0.6rem";
+  suscribeButton.style.height = "2.6rem";
+  suscribeButton.style.marginLeft = "0.5rem"
+}
+  
+function showMenu() {
+  if (menuButton.classList.contains("showMenu")) {
+    menuButton.classList.remove("showMenu");
+    menu.style.display = "block";
+    closeMenu.style.display = "none";
+    navBar.style.display = "none";
+    changeNavBarStyles();
+  } else {
+    menuButton.classList.add("showMenu");
+    menu.style.display = "none";
+    closeMenu.style.display = "block";
+    navBar.style.display = "flex";
+    changeNavBarStyles();
+  }
+}
