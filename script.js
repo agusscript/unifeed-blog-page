bc07606ce742ca53fbf34fc99e9e4863e6a1ba5c
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-01-14 12:45:38 -0300

    Add transition to hamburger menu

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ function changeNavBarStyles() {
   navBar.style.justifyContent = "center";
   navBar.style.backgroundColor = "rgb(29, 29, 29)";
   navBar.style.width = "93%";
-  navBar.style.height = "260px";
+  navBar.style.height = "280px";
   navBar.style.textAlign = "center";
   navBar.style.transform = "translateY(12px)";
+  navBar.style.animation = "fadeIn 0.8s";
   navList.style.flexDirection = "column";
   suscribeButton.style.padding = "1.5rem";
   suscribeButton.style.paddingTop = "0.6rem";
